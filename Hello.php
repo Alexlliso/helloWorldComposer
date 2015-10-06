1e73c57736061508cadada60ee38315a44592434
balrog <alexandrelliso@iesebre.com>
2015-10-06 19:41:38 +0200

    Helloworldcomposer

---
+++ Hello.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: balrog
+ * Date: 6/10/15
+ * Time: 19:31
+ */
+class Hello
+{
+    public function sayHello()
+    {
+        echo "Hola món!!\n";
+    }
+}
---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . "/Hello.php";
+/**
+ * Created by PhpStorm.
+ * User: balrog
+ * Date: 6/10/15
+ * Time: 19:32
+ */
+$greatings = new Hello();
+
+$greatings->sayHello();
